cefef99e672c1acecd1d35861930afb99d145a5c
16dm <92504263+16dm@users.noreply.github.com>
2026-04-18 17:06:10 -0700

    Nevermind....

---
+++ firebase_demo_app/firebase_demo_app/firebase_demo_appApp.swift
@@ -0,0 +1,32 @@
+//
+//  firebase_demo_appApp.swift
+//  firebase_demo_app
+//
+//  Created by Dave Mandi on 4/18/26.
+//
+
+import SwiftUI
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+
+@main
+struct firebase_demo_appApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
